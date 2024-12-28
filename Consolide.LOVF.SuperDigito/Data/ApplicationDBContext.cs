7d0f2736ebef7907fbb264191727ee923a8f867f
luiso <luiso@ITA-L104576>
2024-12-27 19:41:56 -0600

    Agregar archivos de proyecto.

---
+++ Consolide.LOVF.SuperDigito/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Consolide.LOVF.SuperDigito.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Consolide.LOVF.SuperDigito.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasNoKey();
+            modelBuilder.Entity<Historial>().HasNoKey();
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Historial> Historial { get; set; }
+    }
+}
---
+++ Consolide.LOVF.SuperDigito/Models/Historial.cs
@@ -0,0 +1,10 @@
+﻿namespace Consolide.LOVF.SuperDigito.Models
+{
+    public class Historial
+    {
+        public int Id { get; set; }
+        public int Number {  get; set; }
+        public int Result {  get; set; }
+        public DateTime Date { get; set; }
+    }
+}
